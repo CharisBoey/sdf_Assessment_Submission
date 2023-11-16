3dd7d0547b6567ed7f4d3095c833dd9509e6f2b5
CharisBoey <charis_bsy@outlook.com>
2023-11-16 17:02:01 +0800

    FinalTask02

--- task02/src/Task02/Client.java
+++ task02/src/Task02/Client.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Client {
 
